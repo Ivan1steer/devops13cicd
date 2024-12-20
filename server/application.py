a52166414a33d6de96dbccfa2bb21ff4b78eb942
Ivan <vanechka20032001@gmail.com>
2024-12-20 04:26:58 +0000

    fix

--- server/application.py
+++ server/application.py
@@ -2,12 +2,15 @@ import http.server
 import socketserver
 PORT = 8000
 class TestMe():
-	def take_five(self):
-		return 4
-	def port(self):
-		return PORT
+"""Класс для тестирования функциональности сервера."""
+    def take_five(self):
+        """Возвращает число 4."""
+        return 4
+    def port(self):
+        """Возвращает номер порта."""
+        return PORT
 if __name__ == '__main__':
-	Handler = http.server.SimpleHTTPRequestHandler
-	with socketserver.TCPServer(("", PORT), Handler) as http:
-		print("serving at port", PORT)
-		http.serve_forever()
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", PORT), Handler) as http:
+        print("serving at port", PORT)
+        http.serve_forever()
